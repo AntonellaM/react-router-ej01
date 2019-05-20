82db371ab81aac29178be0eda052104be0611b11
marinarodriguezandres <marinarodriguezandres@gmail.com>
2019-05-20 18:53:36 +0200

    Add ex1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Pricing from './components/Pricing';
 
-function App() {
+class App extends React.Component {
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <header>
+       <nav>
+         <ul>
+           <li> <Link to="/">Home </Link></li>
+           <li> <Link to="/about">About </Link></li>
+           <li> <Link to="/pricing">Pricing </Link></li>
+         </ul>
+       </nav>
+     </header>
+     <main>
+       <Switch>
+         <Route exact path="/" component={Home} />
+         <Route path="/about" component={About} />
+         <Route path="/pricing" component={Pricing} />
+       </Switch>
+     </main>
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About(props){
+    return(
+        <React.Fragment>
+        <h2>ABOUT</h2>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt possimus nihil harum veritatis aliquid quas asperiores ipsam cum. Quia dolor ducimus nisi nemo, laboriosam dolorem possimus. Quo magnam eius dolorem?</p>
+        </React.Fragment>
+    )
+
+}
+export default About;
